1bd5d4bc56fb7b9af4a613cf50813cb97a17aa12
Vaijyant Tomar <vaijyant.tomar@gmail.com>
2018-02-21 12:53:23 -0500

    Approach 1

--- src/io/github/vaijyant/Main.java
+++ src/io/github/vaijyant/Main.java
@@ -3,6 +3,12 @@ package io.github.vaijyant;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //Problem 1:
+        String[] dictionary = {"able", "ale", "apple", "bale", "kangaroo"};
+        String  string = "abppplee";
+	    new LongestWord(dictionary, string).identifySubSequence();
+
+
     }
 }
